53c7d1f8a8d153945c32fb46d3614f02449270dd
kriskris79 <krzysztofkakolewski@googlemail.com>
2025-01-04 02:15:39 +0000

    Fixed time tracking across logins, improved date handling, and adjusted alerts for precision. Now logs PowerShell messages and errors in script_output.txt.

--- DailyUsageLimit.bat
+++ DailyUsageLimit.bat
@@ -9,24 +9,39 @@ set logfile=%UserProfile%\Documents\usage.log
 set timelog=%UserProfile%\Documents\timelog.txt
 set scriptLog=%UserProfile%\Documents\script_log.txt
 
-:: Log the start time
-echo [%DATE% %TIME%] Starting script >> "%scriptLog%"
+:: Normalize the current date to YYYYMMDD format for comparison
+for /f "tokens=2 delims==" %%A in ('"wmic os get LocalDateTime /value"') do set ldt=%%A
+set "currentdate=!ldt:~0,8!"
+
+:: Convert YYYYMMDD to DD/MM/YYYY for logging
+set "logdate=!ldt:~6,2!/!ldt:~4,2!/!ldt:~0,4!"
+
+:: Log the start time with formatted date
+echo [%logdate% %TIME%] Starting script >> "%scriptLog%"
 
 :: Check if the computer was used today
-if exist %logfile% (
+if exist "%logfile%" (
     set /p lastdate=<%logfile%
+    :: Remove any carriage return and newline characters
+    set "lastdate=!lastdate:~0,-1!"
+    echo Last use date read from file: !lastdate! >> "%scriptLog%"
 ) else (
-    set lastdate=
+    echo No last use date found. Assuming new installation. >> "%scriptLog%"
+    set "lastdate=00000000"
 )
 
-:: Compare last use date with current date
-if "%lastdate%"=="%date%" (
-    :: Read the total time if the same day
+:: Compare last use date with current normalized date
+echo Comparing date !lastdate! with !currentdate! >> "%scriptLog%"
+if "!lastdate!"=="!currentdate!" (
+    echo Same day detected, reading total time from file. >> "%scriptLog%"
     set /p totaltime=<%timelog%
+    if not defined totaltime set "totaltime=0"
+    echo Resuming with total time: !totaltime! seconds from file >> "%scriptLog%"
 ) else (
-    :: Reset if a new day
-    set /a totaltime=0
-    echo %date% > %logfile%
+    echo New day detected. Resetting total time. >> "%scriptLog%"
+    set "totaltime=0"
+    echo !currentdate! > %logfile%
+    echo 0 > %timelog%
 )
 
 echo Read total time: !totaltime! >> "%scriptLog%"
@@ -50,22 +65,20 @@ if !totaltime! geq !maxtime! goto endsession
 timeout /t 1 /nobreak
 set /a sessiontime+=1
 set /a totaltime+=1
-
-:: Update the timelog every minute
 echo !totaltime! > %timelog%
 
 :: Show messages at different times
-if !sessiontime! equ 15 (
+if !sessiontime! equ 1500 (
     echo 25 minute warning >> "%scriptLog%"
-    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "Children, you have 5 minutes left before you will have to switch the laptops." 15
+    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "You have 5 minutes left before you will have to switch the laptops." 15
 )
 if !sessiontime! equ 1800 (
     echo Half hour warning >> "%scriptLog%"
-    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "Children, time to switch the laptops." 15
+    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "It's time to switch the laptops." 15
 )
 if !sessiontime! equ 3300 (
     echo 55 minute warning >> "%scriptLog%"
-    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "Children, you have 5 minutes left before the computer will switch off." 15
+    PowerShell -File "%UserProfile%\Documents\ShowTimedMessage.ps1" "You have 5 minutes left before the computer will switch off." 15
 )
 if !sessiontime! geq 3600 goto endsession
 goto timer
